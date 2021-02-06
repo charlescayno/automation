0e1817bb0165543978f16d26ecf73c1da019e197
charlescayno <cfcayno@gmail.com>
2021-02-06 14:46:27 +0800

    update

--- codes/gui.py
+++ codes/gui.py
@@ -1,15 +1,43 @@
-import tkinter as tk
-root = tk.Tk()
-root.geometry('400x200')
+# python program demonstrating 
+# Combobox widget using tkinter 
 
-def button_command():
-    text = entry1.get()
-    print(text)
-    return None
 
-entry1 = tk.Entry(root, width = 20)
-entry1.pack()
+import tkinter as tk 
+from tkinter import ttk 
 
-tk.Button(root, text = "Button", command=button_command).pack()
+# Creating tkinter window 
+window = tk.Tk() 
+window.title('Pang-automate') 
+window.geometry('500x250') 
 
-root.mainloop()
+# label text for title 
+ttk.Label(window, text = "I-automate mo na yan", 
+		background = 'green', foreground ="white", 
+		font = ("Times New Roman", 15)).grid(row = 0, column = 1) 
+
+# label 
+ttk.Label(window, text = "Select Test:", 
+		font = ("Times New Roman", 10)).grid(column = 0, 
+		row = 5, padx = 10, pady = 25) 
+
+# Combobox creation 
+n = tk.StringVar() 
+monthchoosen = ttk.Combobox(window, width = 27, textvariable = n) 
+
+# Adding combobox drop down list 
+monthchoosen['values'] = (' Output Startup', 
+						' February', 
+						' March', 
+						' April', 
+						' May', 
+						' June', 
+						' July', 
+						' August', 
+						' September', 
+						' October', 
+						' November', 
+						' December') 
+
+monthchoosen.grid(column = 1, row = 5) 
+monthchoosen.current() 
+window.mainloop() 
