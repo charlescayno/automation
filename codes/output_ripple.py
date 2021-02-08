cba769bce578c7907bdeb3b9eadfc4dc2737a984
charlescayno <cfcayno@gmail.com>
2021-02-09 07:45:18 +0800

    update

--- codes/output_ripple.py
+++ codes/output_ripple.py
@@ -1,8 +1,6 @@
-# git config --unset credential.helper
 # USER INPUT STARTS HERE
 #########################################################################################
 # TEST PARAMETERS
-# TODO: Load the desired Output Voltage Ripple dfl
 # set trigger settings
 trigger_level = 0.01   # [V] starting trigger level
 trigger_source = 1     # CH1
@@ -23,54 +21,89 @@ IC = 'SEC#4 (FAB)'
 # IC = 'LAPISS2#33 (CTRL)'
 #########################################################################################
 # USER INPUT ENDS HERE
-
+# from powi.equipment import ACSource, PowerMeter, ElectronicLoad, Oscilloscope, truncate
+from powi.equipment import Oscilloscope
 from time import sleep, time
 import os
 
-def init_equipment():
-  from powi.equipment import ACSource, PowerMeter, ElectronicLoad, Oscilloscope, truncate
-  ac = ACSource(address=5)
-  pms = PowerMeter(address=1)
-  pml = PowerMeter(address=4)
-  eload = ElectronicLoad(address=16)
-  scope = Oscilloscope(address='10.125.10.139')
+# initialize equipment
+# ac = ACSource(address=5)
+# pms = PowerMeter(address=1)
+# pml = PowerMeter(address=4)
+# eload = ElectronicLoad(address=16)
+scope = Oscilloscope(address='10.125.10.139') # charles
+# scope = Oscilloscope(address='10.125.10.156') # joshua
+
+# initialize variables
+global Iout_index
+global waveform_counter
+global start
+global waveforms_folder
+waveform_counter = 0
+Iout_index = 0
 
 def headers(test_name):
 
-  print()
-  print("="*50)
-  print(f"Test: {test_name}")
-  
-  # initialization
-  global waveform_counter
-  global Iout_index
-  global start
-  waveform_counter = 0
-  Iout_index = 0
+    print()
+    print("="*50)
+    print(f"Test: {test_name}")
 
-  create_folder(test_name)
-  start = time()
-  print()
+    create_folder(test_name)
+    start = time()
+    print()
 
 def create_folder(test_name):
-  global waveforms_folder
-  # creating folder for the saved waveforms
-  waveforms_folder = f'waveforms/{test_name}'
-  pathname = f"{os.getcwd()}\{waveforms_folder}"
-  isExist = os.path.exists(pathname)
+    # creating folder for the saved waveforms
+    waveforms_folder = f'waveforms/{test_name}'
+    pathname = f"{os.getcwd()}\{waveforms_folder}"
+    isExist = os.path.exists(pathname)
 
-  if isExist == False:
-    os.mkdir(pathname)
-    print(f"{waveforms_folder} created.")
-  else:
-    print(f"{waveforms_folder} folder already exists.")
+    if isExist == False:
+        os.mkdir(pathname)
+        print(f"{waveforms_folder} created.")
+    else:
+        print(f"{waveforms_folder} folder already exists.")
 
 def footers():
-  print(f'{waveform_counter} waveforms captured.')
-  print('test complete.')
-  print()
-  end = time()
-  print(f'test time: {(end-start)/60} mins.')
+    print(f'{waveform_counter} waveforms captured.')
+    print('test complete.')
+    print()
+    end = time()
+    print(f'test time: {(end-start)} secs.')
+
+def init_trigger():
+    # Trigger Settings
+    scope.edge_trigger(trigger_source, trigger_level, trigger_slope='POS')
+    scope.trigger_mode(mode='NORM')
+    scope.stop()
+
+def reset():
+    # print("resetting...")
+    ac.turn_off()
+    eload.channel[1].cc = 1
+    eload.channel[1].turn_on()
+    eload.channel[2].cc = 1
+    eload.channel[2].turn_on()
+    sleep(1)
+    print()
+
+def soak(soak_time):
+    for seconds in range(soak_time, 0, -1):
+        sleep(1)
+        print(f"{seconds:5d}s", end="\r")
+    print("       ", end="\r")
+
+## special functions
+
+
+def reminders():
+    print()
+    print("Test Setup")
+    print("> Load .dfl for Output Ripple")
+    print("> Set CH1 = Output Voltage (Barrel Probe) x10 setting")
+    print("Use barrel probe with filter capacitors")
+    scope.time_position(50)
+    input("Press ENTER to continue...")
 
 def find_trigger():
   # finding trigger level
@@ -80,22 +113,20 @@ def find_trigger():
   # get initial peak-to-peak measurement value
   labels, values = scope.get_measure()
   ptp_value = float(values[1])
-  # ptp_value = truncate(ptp_value, 4)
   ptp_value = float(f"{ptp_value:.4f}")
   max_value = float(values[0])
-  # max_value = truncate(max_value, 4)
   max_value = float(f"{max_value:.4f}")
 
   # set max_value as initial trigger level
   trigger_level = max_value
   scope.trigger_level(trigger_source, trigger_level)
 
-  # check initial trigger status
+  # check if it triggered within 5 seconds
   scope.run_single()
   soak(5)
   trigger_status = scope.trigger_status()
 
-  # increase trigger level until it reaches the maximum
+  # increase trigger level until it reaches the maximum trigger level
   while (trigger_status == 1):
     trigger_level = float(trigger_level) + trigger_delta
     scope.trigger_level(trigger_source, trigger_level)
@@ -105,125 +136,49 @@ def find_trigger():
     soak(3)
     trigger_status = scope.trigger_status()
 
-  # decrease one trigger level below to get the maximum trigger possible
+  # decrease trigger level below to get the maximum trigger possible
   trigger_level = float(trigger_level) - 2*trigger_delta
   final_trigger_level = trigger_level
   scope.trigger_level(trigger_source, trigger_level)
 
-def init_trigger():
-  # Trigger Settings
-  scope.edge_trigger(trigger_source, trigger_level, trigger_slope='POS')
-  scope.trigger_mode(mode='NORM')
-  scope.stop()
-
-def get_screenshot():
-  global Iout_index
-  global waveform_counter
-  global waveforms_folder
-  
-  # get screenshot
-  scope.run_single()
-  soak(6)
-  filename = f'{IC} {voltage}Vac {Iout_name[Iout_index]}Load.png'
-  scope.get_screenshot(filename, waveforms_folder)
-  print(f'{IC} {voltage}Vac {Iout_name[Iout_index]}Load.png')
-  
-  Iout_index += 1
-  waveform_counter += 1
-
-def reset_trigger_level():
-  scope.trigger_level(trigger_source, trigger_level = 0.010)
-
-def reset():
-  global Iout_index
-  Iout_index = 0
-  ac.turn_off()
-  eload.channel[1].cc = 1
-  eload.channel[1].turn_on()
-  eload.channel[2].cc = 1
-  eload.channel[2].turn_on()
-  soak(5)
-  print()
-
 def percent_load():
 
-  init_trigger()
+    init_trigger()
 
-  global voltage
-  global frequency
-  for voltage, frequency in zip(vin, freq):
-    
-    ac.voltage = voltage
-    ac.frequency = frequency
-    ac.turn_on()
-    
-    for x in Iout:
-      eload.channel[1].cc = x
-      eload.channel[1].turn_on()
-      
-      if x == 0:
-        soak(10)
-      else:
-        soak(5)
-      
-      find_trigger()
-      scope.run_single()
-      soak(6)
-      get_screenshot()
-      reset_trigger_level()
-      print()
-    reset()
+    for voltage, frequency in zip(vin, freq):
 
-def soak(soak_time):
-  for seconds in range(soak_time, 0, -1):
-      sleep(1)
-      print(f"{seconds:5d}s", end="\r")
-  print("       ", end="\r")
+        ac.voltage = voltage
+        ac.frequency = frequency
+        ac.turn_on()
 
-def test_line_regulation(input_list, soak_time):
-  
-  headers("Line Regulation")
+        for x in Iout:
+            eload.channel[1].cc = x
+            eload.channel[1].turn_on()
+            
+            if x == 0:
+            soak(10)
+            else:
+            soak(5)
+            
+            find_trigger()
 
-  print("Vac, Freq, Vin, Iin, Pin, PF, %THD, Vo1, Io1, Po1, Vreg1, Eff")
+            # get screenshot
+            scope.run_single()
+            soak(6)
+            filename = f'{IC} {voltage}Vac {Iout_name[Iout_index]}Load.png'
+            scope.get_screenshot(filename, waveforms_folder)
+            print(f'{IC} {voltage}Vac {Iout_name[Iout_index]}Load.png')
+            Iout_index += 1
+            waveform_counter += 1
 
-  for voltage, frequency in input_list:
-    ac.voltage = voltage
-    ac.frequency = frequency
-    ac.turn_on()
-
-    eload.channel[1].cc = 1
-    eload.channel[1].turn_on()
-
-    soak(soak_time)
-
-    # create output list
-    vac = str(voltage)
-    freq = str(frequency)
-    vin = f"{pms.voltage:.2f}"
-    iin = f"{pms.current*1000:.2f}"
-    pin = f"{pms.power:.3f}"
-    pf = f"{pms.pf:.4f}"
-    thd = f"{pms.thd:.2f}"
-    vo1 = f"{pml.voltage:.3f}"
-    io1 = f"{pml.current*1000:.2f}"
-    po1 = f"{pml.power:.3f}"
-    vreg1 = f"{100*(float(vo1)-12)/12:.4f}"
-    eff = f"{100*(float(po1))/float(pin):.4f}"
-
-    output_list = [vac, freq, vin, iin, pin, pf, thd, vo1, io1, po1, vreg1, eff]
-
-    print(','.join(output_list))
-
-  reset()
-  footers()
+            # reset trigger level
+            scope.trigger_level(trigger_source, trigger_level = 0.010)
+            print()
+        
+        reset()
 
 ## main code ##
-# init_equipment()
+# reminders()
 # headers("Output Ripple")
 # percent_load()
 # footers()
-
-
-# input_list = [(90, 60), (100, 60), (115, 60), (130, 60), (150, 60), (180, 60), (200, 50), (220, 50), (230, 50), (240, 50), (250, 60), (265, 50)]
-# soak_time = 5
-# test_line_regulation(input_list, soak_time)
--- codes/startup_output.py
+++ codes/startup_output.py
@@ -18,7 +18,6 @@ IC = 'test'
 # IC = 'LAPISS2#33 (CTRL)'
 #########################################################################################
 # USER INPUT ENDS HERE
-
 # from powi.equipment import ACSource, PowerMeter, ElectronicLoad, Oscilloscope, truncate
 from powi.equipment import Oscilloscope
 from time import sleep, time
@@ -42,55 +41,54 @@ Iout_index = 0
 
 def headers(test_name):
 
-  print()
-  print("="*50)
-  print(f"Test: {test_name}")
+    print()
+    print("="*50)
+    print(f"Test: {test_name}")
 
-  create_folder(test_name)
-  start = time()
-  print()
+    create_folder(test_name)
+    start = time()
+    print()
 
 def create_folder(test_name):
-  # creating folder for the saved waveforms
-  waveforms_folder = f'waveforms/{test_name}'
-  pathname = f"{os.getcwd()}\{waveforms_folder}"
-  isExist = os.path.exists(pathname)
+    # creating folder for the saved waveforms
+    waveforms_folder = f'waveforms/{test_name}'
+    pathname = f"{os.getcwd()}\{waveforms_folder}"
+    isExist = os.path.exists(pathname)
 
-  if isExist == False:
-    os.mkdir(pathname)
-    print(f"{waveforms_folder} created.")
-  else:
-    print(f"{waveforms_folder} folder already exists.")
+    if isExist == False:
+        os.mkdir(pathname)
+        print(f"{waveforms_folder} created.")
+    else:
+        print(f"{waveforms_folder} folder already exists.")
 
 def footers():
-  print(f'{waveform_counter} waveforms captured.')
-  print('test complete.')
-  print()
-  end = time()
-  print(f'test time: {(end-start)} secs.')
+    print(f'{waveform_counter} waveforms captured.')
+    print('test complete.')
+    print()
+    end = time()
+    print(f'test time: {(end-start)} secs.')
 
 def init_trigger():
-  # Trigger Settings
-  scope.edge_trigger(trigger_source, trigger_level, trigger_slope='POS')
-  scope.trigger_mode(mode='NORM')
-  scope.stop()
+    # Trigger Settings
+    scope.edge_trigger(trigger_source, trigger_level, trigger_slope='POS')
+    scope.trigger_mode(mode='NORM')
+    scope.stop()
 
 def reset():
-  # print("resetting...")
-  ac.turn_off()
-  eload.channel[1].cc = 1
-  eload.channel[1].turn_on()
-  eload.channel[2].cc = 1
-  eload.channel[2].turn_on()
-  sleep(1)
-  print()
+    # print("resetting...")
+    ac.turn_off()
+    eload.channel[1].cc = 1
+    eload.channel[1].turn_on()
+    eload.channel[2].cc = 1
+    eload.channel[2].turn_on()
+    sleep(1)
+    print()
 
 def soak(soak_time):
-  for seconds in range(soak_time, 0, -1):
-      sleep(1)
-      print(f"{seconds:5d}s", end="\r")
-  print("       ", end="\r")
-
+    for seconds in range(soak_time, 0, -1):
+        sleep(1)
+        print(f"{seconds:5d}s", end="\r")
+    print("       ", end="\r")
 
 
 
@@ -99,139 +97,137 @@ def soak(soak_time):
 #### special functions #####
 
 def reminders():
-  print()
-  print("Test Setup")
-  print("> Load .dfl for Output Startup")
-  print("> Set CH1 = Input Voltage (Diff Probe) x100 setting")
-  print("> Set CH2 = Output Voltage (Barrel Probe) x10 setting")
-  print("> Set CH3 = Output Current (Current Probe)")
-  print()
-  print("> Set position to 50%")
-  scope.time_position(50)
-  print("> Set the trigger level to the voltage output regulation")
-  print()
-  input("Press ENTER to continue...")
+    print()
+    print("Test Setup")
+    print("> Load .dfl for Output Startup")
+    print("> Set CH1 = Input Voltage (Diff Probe) x100 setting")
+    print("> Set CH2 = Output Voltage (Barrel Probe) x10 setting")
+    print("> Set CH3 = Output Current (Current Probe)")
+    print()
+    print("> Set position to 50%")
+    scope.time_position(50)
+    print("> Set the trigger level to the voltage output regulation")
+    print()
+    input("Press ENTER to continue...")
 
 def startup_90degPhase(voltage,frequency):
-  # ac.write("OUTP:STAT?")
-  # ac.write("SYST:ERR?")
-  ac.voltage = 0
-  ac.frequency = frequency
-  # ac.write("OUTP ON")
-  ac.turn_on()
-  ac.write("TRIG:TRAN:SOUR BUS")
-  ac.write("ABORT")
-  ac.write("LIST:DWEL 1, 1, 1")
-  
-  ac.write("VOLT:MODE LIST")
-  ac.write(f"LIST:VOLT {voltage}, {voltage}, {voltage}")
-  ac.write("VOLT:SLEW:MODE LIST")
-  ac.write("LIST:VOLT:SLEW 9.9e+037, 9.9e+037, 9.9e+037")
-  
-  ac.write("FREQ:MODE LIST")
-  ac.write(f"LIST:FREQ {frequency}, {frequency}, {frequency}")
-  ac.write("FREQ:SLEW:MODE LIST")
-  ac.write("LIST:FREQ:SLEW 9.9e+037, 9.9e+037, 9.9e+037")
+    # ac.write("OUTP:STAT?")
+    # ac.write("SYST:ERR?")
+    ac.voltage = 0
+    ac.frequency = frequency
+    # ac.write("OUTP ON")
+    ac.turn_on()
+    ac.write("TRIG:TRAN:SOUR BUS")
+    ac.write("ABORT")
+    ac.write("LIST:DWEL 1, 1, 1")
 
-  ac.write("VOLT:OFFS:MODE FIX")
-  ac.write("VOLT:OFFS:SLEW:MODE FIX")
-  
-  ac.write("PHAS:MODE LIST")
-  # ac.write("LIST:PHAS 270, 270, 270")
-  ac.write("LIST:PHAS 90, 90, 90")
-  
-  ac.write("CURR:PEAK:MODE LIST")
-  ac.write("LIST:CURR 40.4, 40.4, 40.4")
-  
-  ac.write("FUNC:MODE FIX")
-  ac.write("LIST:TTLT ON,OFF,OFF")
-  ac.write("LIST:STEP AUTO")
-  # ac.write("SYST:ERR?")
+    ac.write("VOLT:MODE LIST")
+    ac.write(f"LIST:VOLT {voltage}, {voltage}, {voltage}")
+    ac.write("VOLT:SLEW:MODE LIST")
+    ac.write("LIST:VOLT:SLEW 9.9e+037, 9.9e+037, 9.9e+037")
 
-  ac.write("OUTP:TTLT:STAT ON")
-  ac.write("OUTP:TTLT:SOUR LIST")
-  ac.write("TRIG:SYNC:SOUR PHASE")
-  ac.write("TRIG:SYNC:PHAS 0.0")
-  ac.write("TRIG:TRAN:DEL 0")
-  ac.write("Sens:Swe:Offs:Poin 0")
-  ac.write("TRIG:ACQ:SOUR TTLT")
-  ac.write("INIT:IMM:SEQ3")
-  ac.write("LIST:COUN 1")
-  ac.write("INIT:IMM:SEQ1")
-  ac.write("TRIG:TRAN:SOUR BUS")
-  # ac.write("SYST:ERR?")
+    ac.write("FREQ:MODE LIST")
+    ac.write(f"LIST:FREQ {frequency}, {frequency}, {frequency}")
+    ac.write("FREQ:SLEW:MODE LIST")
+    ac.write("LIST:FREQ:SLEW 9.9e+037, 9.9e+037, 9.9e+037")
 
-  ac.write("TRIG:IMM")
+    ac.write("VOLT:OFFS:MODE FIX")
+    ac.write("VOLT:OFFS:SLEW:MODE FIX")
+
+    ac.write("PHAS:MODE LIST")
+    # ac.write("LIST:PHAS 270, 270, 270")
+    ac.write("LIST:PHAS 90, 90, 90")
+
+    ac.write("CURR:PEAK:MODE LIST")
+    ac.write("LIST:CURR 40.4, 40.4, 40.4")
+
+    ac.write("FUNC:MODE FIX")
+    ac.write("LIST:TTLT ON,OFF,OFF")
+    ac.write("LIST:STEP AUTO")
+    # ac.write("SYST:ERR?")
+
+    ac.write("OUTP:TTLT:STAT ON")
+    ac.write("OUTP:TTLT:SOUR LIST")
+    ac.write("TRIG:SYNC:SOUR PHASE")
+    ac.write("TRIG:SYNC:PHAS 0.0")
+    ac.write("TRIG:TRAN:DEL 0")
+    ac.write("Sens:Swe:Offs:Poin 0")
+    ac.write("TRIG:ACQ:SOUR TTLT")
+    ac.write("INIT:IMM:SEQ3")
+    ac.write("LIST:COUN 1")
+    ac.write("INIT:IMM:SEQ1")
+    ac.write("TRIG:TRAN:SOUR BUS")
+    # ac.write("SYST:ERR?")
+
+    ac.write("TRIG:IMM")
 
 def startup_cc():
 
-  init_trigger()
-  scope.trigger_level(trigger_source, trigger_level) 
+    init_trigger()
+    scope.trigger_level(trigger_source, trigger_level) 
 
-  for voltage, frequency in zip(vin, freq):
-    
+    for voltage, frequency in zip(vin, freq):
+
     # set input voltage
     ac.voltage = voltage
     ac.frequency = frequency
-    
+
     for curr in Iout:
-      
-      # set eload
-      eload.channel[1].cc = curr
-      eload.channel[1].turn_on()
+        
+        # set eload
+        eload.channel[1].cc = curr
+        eload.channel[1].turn_on()
 
-      # trigger scope
-      scope.run_single()
-      sleep(2)
-      startup_90degPhase(voltage, frequency)
-      sleep(5)
-      scope.stop()
+        # trigger scope
+        scope.run_single()
+        sleep(2)
+        startup_90degPhase(voltage, frequency)
+        sleep(5)
+        scope.stop()
 
-      # get waveform data from scope
-      vin_data = scope.get_chan_data(1)
-      vo_data = scope.get_chan_data(2)
+        # get waveform data from scope
+        vin_data = scope.get_chan_data(1)
+        vo_data = scope.get_chan_data(2)
 
-      # search algorithm for cursor 1
-      j = 0
-      pos_x1 = 0
-      for point in vin_data:
+        # search algorithm for cursor 1
+        j = 0
+        pos_x1 = 0
+        for point in vin_data:
         if point >= 10: # if vin >= 10V
-          pos_x1 = j    # set cursor here
-          break
+            pos_x1 = j    # set cursor here
+            break
         j += 1
-      
-      # search algorithm for cursor 2
-      j = 0
-      pos_x1 = 0
-      for point in vin_data:
+        
+        # search algorithm for cursor 2
+        j = 0
+        pos_x1 = 0
+        for point in vin_data:
         if point >= trigger_level: # if vo >= vo_reg
-          pos_x1 = j               # set cursor there
-          break
+            pos_x1 = j               # set cursor there
+            break
         j += 1
 
-      # set cursors (to get startup time)
-      a = scope.get_horizontal()
-      resolution = float(a["resolution"])
-      minimum = float(a["scale"])*(-5) # set the cursor to the leftmost part of the screen
-      cursor1 = minimum + resolution*pos_x1
-      cursor2 = minimum + resolution*pos_x2
-      scope.cursor(channel=1, cursor_set=1, X1=cursor1, X2=cursor2)
-      startup_time = scope.get_cursor()["delta x"]
-      print(f"startup time = {startup_time} s")
+        # set cursors (to get startup time)
+        a = scope.get_horizontal()
+        resolution = float(a["resolution"])
+        minimum = float(a["scale"])*(-5) # set the cursor to the leftmost part of the screen
+        cursor1 = minimum + resolution*pos_x1
+        cursor2 = minimum + resolution*pos_x2
+        scope.cursor(channel=1, cursor_set=1, X1=cursor1, X2=cursor2)
+        startup_time = scope.get_cursor()["delta x"]
+        print(f"startup time = {startup_time} s")
 
-      # get screenshot
-      sleep(1)
-      filename = f'{IC} {voltage}Vac {Iout_name[Iout_index]}LoadCC.png'
-      scope.get_screenshot(filename, waveforms_folder)
-      print(f'{IC} {voltage}Vac {Iout_name[Iout_index]}LoadCC.png')
-      Iout_index += 1
-      waveform_counter += 1
+        # get screenshot
+        sleep(1)
+        filename = f'{IC} {voltage}Vac {Iout_name[Iout_index]}LoadCC.png'
+        scope.get_screenshot(filename, waveforms_folder)
+        print(f'{IC} {voltage}Vac {Iout_name[Iout_index]}LoadCC.png')
+        Iout_index += 1
+        waveform_counter += 1
 
-      reset()
-    
+        reset()
+
     Iout_index = 0 # resest iout naming index for the next voltage
-
-
 
 
 
