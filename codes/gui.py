c0f8ee4254e56c4165cfdfea8d3d78cb73f2f142
unknown <cfcayno@gmail.com>
2021-02-10 16:58:06 +0800

    update

--- codes/gui.py
+++ codes/gui.py
@@ -7,37 +7,30 @@ from tkinter import ttk
 
 # Creating tkinter window 
 window = tk.Tk() 
-window.title('Pang-automate') 
+window.title('Automation') 
 window.geometry('500x250') 
 
 # label text for title 
-ttk.Label(window, text = "I-automate mo na yan", 
+ttk.Label(window, text = "Automation Tests", 
 		background = 'green', foreground ="white", 
-		font = ("Times New Roman", 15)).grid(row = 0, column = 1) 
+		font = ("Calibri", 15)).grid(row = 0, column = 1) 
 
 # label 
 ttk.Label(window, text = "Select Test:", 
-		font = ("Times New Roman", 10)).grid(column = 0, 
+		font = ("Calibri", 10)).grid(column = 0, 
 		row = 5, padx = 10, pady = 25) 
 
 # Combobox creation 
 n = tk.StringVar() 
-monthchoosen = ttk.Combobox(window, width = 27, textvariable = n) 
+testselection = ttk.Combobox(window, width = 27, textvariable = n) 
 
 # Adding combobox drop down list 
-monthchoosen['values'] = (' Output Startup', 
-						' February', 
-						' March', 
-						' April', 
-						' May', 
-						' June', 
-						' July', 
-						' August', 
-						' September', 
-						' October', 
-						' November', 
-						' December') 
+testselection['values'] = (' Output Startup Waveforms', 
+						' Output Bench Ripple Waveforms') 
 
-monthchoosen.grid(column = 1, row = 5) 
-monthchoosen.current() 
-window.mainloop() 
+testselection.grid(column = 1, row = 5) 
+testselection.current()
+
+
+
+window.mainloop()
